9cd205d2bc037c085b70149e02e749e70feea1e6
Simon Plourde <splourde@lapresse.ca>
2014-05-15 20:41:00 -0400

    Use https requests to API when enabled

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
 /**
  * Authentification
