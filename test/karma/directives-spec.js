a39e0f35d632780368355eaa96d5c5008cc753f8
Justin Kolberg <amd.prophet@gmail.com>
2014-09-26 16:06:23 -0700

    remove bootstrapTooltip directive karma tests

--- test/karma/directives-spec.js
+++ test/karma/directives-spec.js
@@ -10,23 +10,6 @@ describe('directives', function () {
     element = jasmine.createSpyObj('element', ['tooltip', 'attr', 'removeAttr', 'addClass']);
   }));
 
-  describe('bootstrapTooltip', function () {
-
-    it('should be restricted to elements and attributes', inject(function (bootstrapTooltipDirective) {
-      expect(bootstrapTooltipDirective[0].restrict).toBe('EA');
-    }));
-
-    it('should have a link method', inject(function (bootstrapTooltipDirective) {
-      expect(bootstrapTooltipDirective[0].link).toBeDefined();
-    }));
-
-    it('should call element.tooltip() when calling link', inject(function (bootstrapTooltipDirective) {
-      bootstrapTooltipDirective[0].link(scope, element);
-      expect(element.tooltip).toHaveBeenCalled();
-    }));
-
-  });
-
   describe('siteTheme', function () {
 
     it('should be restricted to elements and attributes', inject(function (siteThemeDirective) {
