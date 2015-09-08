6caec4e30e59e6397db9168175492e52764218c4
Simon Plourde <simon.plourde@gmail.com>
2015-09-08 18:53:41 -0400

    Add support for LDAP userObjectClass attribute

--- uchiwa/config/config.go
+++ uchiwa/config/config.go
@@ -7,8 +7,8 @@ import (
 	"os"
 	"strings"
 
-	"github.com/sensu/uchiwa/uchiwa/logger"
 	"github.com/sensu/uchiwa/uchiwa/auth"
+	"github.com/sensu/uchiwa/uchiwa/logger"
 )
 
 // Config struct contains []SensuConfig and UchiwaConfig structs
@@ -69,15 +69,16 @@ type Github struct {
 
 // Ldap struct contains the LDAP driver configuration
 type Ldap struct {
-	Server        string
-	Port          int
-	BaseDN        string
-	BindUser      string
-	BindPass      string
-	Insecure      bool
-	Roles         []auth.Role
-	Security      string
-	UserAttribute string
+	Server          string
+	Port            int
+	BaseDN          string
+	BindUser        string
+	BindPass        string
+	Insecure        bool
+	Roles           []auth.Role
+	Security        string
+	UserAttribute   string
+	UserObjectClass string
 }
 
 // Load retrieves a specified configuration file and return a Config struct
@@ -161,6 +162,9 @@ func (c *Config) initUchiwa() {
 		}
 		if c.Uchiwa.Ldap.UserAttribute == "" {
 			c.Uchiwa.Ldap.UserAttribute = "sAMAccountName"
+		}
+		if c.Uchiwa.Ldap.UserObjectClass == "" {
+			c.Uchiwa.Ldap.UserObjectClass = "person"
 		}
 	} else if c.Uchiwa.Db.Driver != "" && c.Uchiwa.Db.Scheme != "" {
 		c.Uchiwa.Auth = "sql"
