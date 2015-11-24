ff0651bdfa95d69cf0535e801a293f257dc2c3e7
Simon Plourde <simon.plourde@gmail.com>
2015-11-24 18:59:49 -0500

    Add support for loading configuration from directories

--- uchiwa/config/config_test.go
+++ uchiwa/config/config_test.go
@@ -3,72 +3,143 @@ package config
 import (
 	"testing"
 
+	"github.com/sensu/uchiwa/uchiwa/auth"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestLoad(t *testing.T) {
-	_, err := Load("../foo.bar")
-	assert.NotNil(t, err, "should return an error when file does not exist")
+	// Only default & config file
+	conf := Load("../../fixtures/config_test.json", "")
+	assert.Equal(t, 4567, conf.Sensu[0].Port)
+	assert.Equal(t, 10, conf.Sensu[0].Timeout)
+	assert.Equal(t, 10, conf.Uchiwa.Refresh)
+	assert.Equal(t, 389, conf.Uchiwa.Ldap.Port)
+	assert.Equal(t, "default", conf.Uchiwa.Audit.Level)
 
-	_, err = Load("../uchiwa.go")
-	assert.NotNil(t, err, "should return an error when it cannot parse a file")
+	conf = Load("../../fixtures/config_test.json", "../../fixtures/conf.d")
+	assert.Equal(t, 5, len(conf.Sensu))
+	assert.Equal(t, 4567, conf.Sensu[0].Port)
+	assert.Equal(t, 10, conf.Sensu[0].Timeout)
+	assert.Equal(t, 4569, conf.Sensu[3].Port)
+	assert.Equal(t, 5, conf.Sensu[3].Timeout)
+	assert.Equal(t, 4567, conf.Sensu[4].Port)
+	assert.Equal(t, 10, conf.Sensu[4].Timeout)
 
-	conf, err := Load("../../fixtures/config_test.json")
-	assert.Nil(t, err, "got unexpected error: %s", err)
-
-	// private config
-	assert.NotEqual(t, "*****", conf.Uchiwa.User, "Uchiwa user in private config shouldn't be masked")
-	assert.NotEqual(t, "*****", conf.Uchiwa.Pass, "Uchiwa pass in private config shouldn't be masked")
-	for i := range conf.Sensu {
-		assert.NotEqual(t, "*****", conf.Sensu[i].User, "Sensu APIs user in private config shouldn't be masked")
-		assert.NotEqual(t, "*****", conf.Sensu[i].Pass, "Sensu APIs pass in private config shouldn't be masked")
-	}
-	assert.Equal(t, 1, len(conf.Uchiwa.Users))
-
-	// public config
-	public := conf.GetPublic()
-	assert.Equal(t, "*****", public.Uchiwa.User, "Uchiwa user in public config should be masked")
-	assert.Equal(t, "*****", public.Uchiwa.Pass, "Uchiwa pass in public config should be masked")
-	for i := range public.Sensu {
-		assert.Equal(t, "*****", public.Sensu[i].User, "Sensu APIs user in public config should be masked")
-		assert.Equal(t, "*****", public.Sensu[i].Pass, "Sensu APIs pass in public config should be masked")
-	}
-	assert.Equal(t, 0, len(public.Uchiwa.Users))
-
-}
-
-func TestLoadArrayOfUsers(t *testing.T) {
-	conf, err := Load("../../fixtures/config_test_multiple.json")
-	assert.Nil(t, err, "got unexpected error: %s", err)
-	assert.NotNil(t, conf, "conf should not be nil")
-
-	assert.Equal(t, "simple", conf.Uchiwa.Auth.Driver, "Uchiwa authentication driver should be 'simple'")
+	assert.Equal(t, "192.168.0.1", conf.Uchiwa.Host)
+	assert.Equal(t, 8000, conf.Uchiwa.Port)
 	assert.Equal(t, 2, len(conf.Uchiwa.Users))
 }
 
-func TestLoadArrayOfUsersOnPublicGet(t *testing.T) {
-	conf, err := Load("../../fixtures/config_test_multiple.json")
-	assert.Nil(t, err, "got unexpected error: %s", err)
-	assert.NotNil(t, conf, "conf should not be nil")
+func TestLoadDirectories(t *testing.T) {
+	conf := loadDirectories("foobar,../../fixtures/conf.d")
 
-	assert.Equal(t, "simple", conf.Uchiwa.Auth.Driver, "Uchiwa authentication driver should be 'simple'")
-	public := conf.GetPublic()
-	assert.Equal(t, 0, len(public.Uchiwa.Users))
+	assert.Equal(t, 3, len(conf.Sensu))
+	assert.Equal(t, "us-east-2", conf.Sensu[0].Name)
+	assert.Equal(t, "us-west-2", conf.Sensu[1].Name)
+	assert.Equal(t, "us-east-3", conf.Sensu[2].Name)
+
+	assert.Equal(t, 2, len(conf.Uchiwa.Users))
+	assert.Equal(t, "admin", conf.Uchiwa.Users[0].Username)
+	assert.Equal(t, "readonly", conf.Uchiwa.Users[1].Username)
+	assert.Equal(t, "192.168.0.1", conf.Uchiwa.Host)
+	assert.Equal(t, 8000, conf.Uchiwa.Port)
+}
+
+func TestLoadFile(t *testing.T) {
+	_, err := loadFile("foo.bar")
+	assert.NotNil(t, err, "foo.bar does not exist")
+
+	_, err = loadFile("config.go")
+	assert.NotNil(t, err, "config.go is not a JSON file")
+
+	conf, err := loadFile("../../fixtures/config_test.json")
+	assert.Nil(t, err, "got unexpected error: %s", err)
+
+	// Sensu APIs
+	assert.Equal(t, 2, len(conf.Sensu))
+	assert.Equal(t, "us-east-1", conf.Sensu[0].Name)
+	assert.Equal(t, "us-west-1", conf.Sensu[1].Name)
+	assert.Equal(t, 4570, conf.Sensu[1].Port)
+	assert.Equal(t, 5, conf.Sensu[1].Timeout)
+
+	// Uchiwa
+	assert.Equal(t, "0.0.0.0", conf.Uchiwa.Host)
+	assert.Equal(t, 8080, conf.Uchiwa.Port)
+	assert.Equal(t, "foo", conf.Uchiwa.User)
+	assert.Equal(t, "bar", conf.Uchiwa.Pass)
 }
 
 func TestInitSensu(t *testing.T) {
-	c := Config{
+	apis := []SensuConfig{
+		SensuConfig{Host: "10.0.0.1", Port: 4567},
+		SensuConfig{Name: "test/1", Host: "10.0.10.1", Port: 4567, Ssl: true},
+	}
+
+	sensu := initSensu(apis)
+	assert.NotEqual(t, "", sensu[0].Name)
+	assert.Equal(t, "http://10.0.0.1:4567", sensu[0].URL)
+	assert.Equal(t, "test1", sensu[1].Name)
+	assert.Equal(t, "https://10.0.10.1:4567", sensu[1].URL)
+}
+
+func TestInitUchiwa(t *testing.T) {
+	conf := GlobalConfig{Github: Github{Server: "127.0.0.1"}}
+	uchiwa := initUchiwa(conf)
+	assert.Equal(t, "github", uchiwa.Auth.Driver)
+
+	conf = GlobalConfig{Ldap: Ldap{BaseDN: "cn=foo", Server: "127.0.0.1"}}
+	uchiwa = initUchiwa(conf)
+	assert.Equal(t, "ldap", uchiwa.Auth.Driver)
+	assert.Equal(t, Ldap{BaseDN: "cn=foo", GroupBaseDN: "cn=foo", UserBaseDN: "cn=foo", Server: "127.0.0.1"}, uchiwa.Ldap)
+
+	conf = GlobalConfig{Db: Db{Driver: "mysql", Scheme: "foo"}}
+	uchiwa = initUchiwa(conf)
+	assert.Equal(t, "sql", uchiwa.Auth.Driver)
+
+	conf = GlobalConfig{Users: []auth.User{auth.User{ID: 1}}}
+	uchiwa = initUchiwa(conf)
+	assert.Equal(t, "simple", uchiwa.Auth.Driver)
+
+	conf = GlobalConfig{User: "foo", Pass: "secret"}
+	uchiwa = initUchiwa(conf)
+	assert.Equal(t, "simple", uchiwa.Auth.Driver)
+	assert.Equal(t, []auth.User{auth.User{ID: 0, FullName: "foo", Password: "secret", Username: "foo"}}, uchiwa.Users)
+}
+
+func TestGetPublic(t *testing.T) {
+	conf := Config{
 		Sensu: []SensuConfig{
-			{Name: "foo ? bar", Host: "127.0.0.1"},
-			{Name: "bar / foo", Host: "127.0.0.1"},
+			SensuConfig{
+				User: "foo",
+				Pass: "secret",
+			},
+		},
+		Uchiwa: GlobalConfig{
+			User:   "foo",
+			Pass:   "secret",
+			Users:  []auth.User{auth.User{ID: 1}},
+			Db:     Db{Scheme: "foo"},
+			Github: Github{ClientID: "foo", ClientSecret: "secret"},
+			Ldap:   Ldap{BindPass: "secret"},
 		},
 	}
 
-	c.initSensu()
+	pubConf := conf.GetPublic()
 
-	expectedConfig := []SensuConfig{
-		{Name: "foo  bar", Host: "127.0.0.1", Port: 4567, Ssl: false, Insecure: false, URL: "http://127.0.0.1:4567", User: "", Path: "", Pass: "", Timeout: 10},
-		{Name: "bar  foo", Host: "127.0.0.1", Port: 4567, Ssl: false, Insecure: false, URL: "http://127.0.0.1:4567", User: "", Path: "", Pass: "", Timeout: 10},
-	}
-	assert.Equal(t, expectedConfig, c.Sensu)
+	assert.NotEqual(t, conf, pubConf)
+
+	assert.Equal(t, "foo", conf.Sensu[0].User)
+	assert.Equal(t, "secret", conf.Sensu[0].Pass)
+	assert.Equal(t, "foo", conf.Uchiwa.User)
+	assert.Equal(t, "secret", conf.Uchiwa.Pass)
+
+	assert.Equal(t, "*****", pubConf.Sensu[0].User)
+	assert.Equal(t, "*****", pubConf.Sensu[0].Pass)
+	assert.Equal(t, "*****", pubConf.Uchiwa.User)
+	assert.Equal(t, "*****", pubConf.Uchiwa.Pass)
+	assert.Equal(t, []auth.User{}, pubConf.Uchiwa.Users)
+	assert.Equal(t, "*****", pubConf.Uchiwa.Db.Scheme)
+	assert.Equal(t, "*****", pubConf.Uchiwa.Github.ClientID)
+	assert.Equal(t, "*****", pubConf.Uchiwa.Github.ClientSecret)
+	assert.Equal(t, "*****", pubConf.Uchiwa.Ldap.BindPass)
 }
---
+++ uchiwa/config/structs.go
@@ -0,0 +1,81 @@
+package config
+
+import (
+	"github.com/sensu/uchiwa/uchiwa/auth"
+	"github.com/sensu/uchiwa/uchiwa/structs"
+)
+
+// Config struct contains []SensuConfig and UchiwaConfig structs
+type Config struct {
+	Dashboard *GlobalConfig `json:",omitempty"`
+	Sensu     []SensuConfig
+	Uchiwa    GlobalConfig
+}
+
+// SensuConfig struct contains conf about a Sensu API
+type SensuConfig struct {
+	Name     string
+	Host     string
+	Port     int
+	Ssl      bool
+	Insecure bool
+	URL      string
+	User     string
+	Path     string
+	Pass     string
+	Timeout  int
+}
+
+// GlobalConfig struct contains conf about Uchiwa
+type GlobalConfig struct {
+	Host       string
+	Port       int
+	Refresh    int
+	Pass       string
+	User       string
+	Users      []auth.User
+	Audit      Audit
+	Auth       structs.Auth
+	Db         Db
+	Enterprise bool
+	Github     Github
+	Ldap       Ldap
+}
+
+// Audit struct contains the config of the Audit logger
+type Audit struct {
+	Level   string
+	Logfile string
+}
+
+// Db struct contains the SQL driver configuration
+type Db struct {
+	Driver string
+	Scheme string
+}
+
+// Github struct contains the GitHub driver configuration
+type Github struct {
+	ClientID     string
+	ClientSecret string
+	Roles        []auth.Role
+	Server       string
+}
+
+// Ldap struct contains the LDAP driver configuration
+type Ldap struct {
+	Server               string
+	Port                 int
+	BaseDN               string
+	BindUser             string
+	BindPass             string
+	GroupBaseDN          string
+	GroupObjectClass     string
+	GroupMemberAttribute string
+	Insecure             bool
+	Roles                []auth.Role
+	Security             string
+	UserAttribute        string
+	UserBaseDN           string
+	UserObjectClass      string
+}
